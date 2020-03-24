1902684052e1b3c03a6fdc7c5acf9e55c6922e49
Rory-Sullivan <rory.sully@hotmail.co.uk>
2020-03-24 16:00:19 +0000

    Dijkstra algorithm inital commit.

---
+++ algorithms/dijkstra.js
@@ -0,0 +1,6 @@
+/**
+ * An implementation of Dijksta's shortest path algorithm.  This implementation
+ * finds the shortest path from a start node to an end node in a hexagonal grid
+ * where each node is separated by a distance of one.
+ */
+class Graph {}
