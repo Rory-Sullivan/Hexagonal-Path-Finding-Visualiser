f632f15b572a03ff46e4918864c7cf3e0a8c4f82
Rory-Sullivan <rory.sully@hotmail.co.uk>
2020-03-25 15:05:35 +0000

    Added test files.

---
+++ tests/script.test.js
@@ -0,0 +1,10 @@
+describe('grid matrix tests', function() {
+    it('iterate over array', function() {
+        let access;
+        for (let i = 0; i < m; i++) {
+            for (let j = 0; j < n; j++) {
+                access = grid[i][j];
+            }
+        }
+    });
+});
